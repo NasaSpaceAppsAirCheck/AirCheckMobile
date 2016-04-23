b31662cd609bbf6ef394b8307b17f2ff4d713f64
Cameron Wardzala <cwardzala@gmail.com>
2016-04-23 17:59:30 -0400

    Clean up some visual cruft (#2)
    * Clean up some visual cruft
    
    * Fix ios rendering issues

--- GASP/www/js/index.js
+++ GASP/www/js/index.js
@@ -1,24 +1,34 @@
-var createLogObject = function (app) {
-    var symptom = document.getElementById('symptomSelect').value,
-        pain = document.getElementById('painRange').value;
-    var now = new Date();
-
-    var data = {
-        symptom: symptom,
-        severity: parseInt(pain, 10),
-        ip: app.ip,
-        location: app.location,
-        time: now.toISOString()
-    };
-
-    console.log(data);
-
-    return data;
+var handleRequestLoad = function (success, fail) {
+    if (this.status >= 200 && this.status < 400) {
+        // Success!
+        var data = JSON.parse(this.response);
+        success(data);
+    } else {
+        // We reached our target server, but it returned an error
+        fail();
+    }
 };
 
-var sendToDB = function (data) {
+// Not a sustainable solution. Don't do this Ever!
+var resetForm = function () {
+    document.getElementById('symptomSelect').options[0].selected = true;
+    document.getElementById('painRange').value = 0;
+};
+
+var sendToDB = function (data, success, fail) {
+    var url = 'http://40.83.188.181:9200/user/symptoms';
     var request = new XMLHttpRequest();
-    request.open('POST', 'http://40.83.188.181:9200/user/symptoms', true);
+    request.open('POST', url, true);
+    request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    request.onload = handleRequestLoad.bind(request, success, fail);
+    request.onerror = function () { fail(); };
+    request.send(JSON.stringify(data));
+};
+
+var sendToLogger = function (data) {
+    var url = 'http://requestb.in/1cidfkx1';
+    var request = new XMLHttpRequest();
+    request.open('POST', url, true);
     request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
     request.send(JSON.stringify(data));
 };
@@ -28,16 +38,7 @@ var getIPAddress = function (success, fail) {
     var request = new XMLHttpRequest();
     request.open('GET', 'http://jsonip.com/', true);
 
-    request.onload = function () {
-        if (this.status >= 200 && this.status < 400) {
-            // Success!
-            var data = JSON.parse(this.response);
-            success(data);
-        } else {
-            // We reached our target server, but it returned an error
-            fail();
-        }
-    };
+    request.onload = handleRequestLoad.bind(request, success, fail);
 
     request.onerror = function () {
         fail();
@@ -77,10 +78,37 @@ var app = {
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var self = this;
+
         document.querySelector('.js-log').addEventListener('click', function () {
-            var data = createLogObject(self);
-            sendToDB(data);
+            var data = self.createLogObject(self);
+
+            sendToDB(data, function () {
+                navigator.notification.alert('Your data has been logged.', function () {
+                    sendToLogger(data);
+                    resetForm();
+                }, 'Thanks!');
+            }, function () {
+                alert('error');
+            });
         }, false);
+
+    },
+
+    createLogObject: function () {
+        var symptom = document.getElementById('symptomSelect').value,
+            pain = document.getElementById('painRange').value;
+
+        var now = new Date();
+
+        var data = {
+            symptom: symptom,
+            severity: parseInt(pain, 10),
+            ip: this.ip,
+            location: this.location,
+            time: now.toISOString()
+        };
+
+        return data;
     }
 };
 
